1c041f948abcab48899d33d37f80729db89114df
maha20514 <maha053222@gmail.com>
2024-12-11 01:47:22 +0300

    Update

--- server/index.js
+++ server/index.js
@@ -13,7 +13,15 @@ import xmlFeedRouter from "./routes/properties-feed.routes.js"; // Import the XM
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+// Configure CORS
+const corsOptions = {
+  origin: ["https://bay-homes-admin.vercel.app"], // Add your dashboard's URL here
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+};
+app.use(cors(corsOptions));
+
 app.use(express.json({ limit: "50mb" }));
 
 app.get("/", (req, res) => {
@@ -29,15 +37,15 @@ app.use("/api/v1/developers", developerRouter);
 // Register the new XML feed routes
 app.use("/api/feed", xmlFeedRouter);
 
-
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL, {
-      dbName: 'bay-homes'
+      dbName: "bay-homes",
     });
 
-    app.listen(8080, () =>
-      console.log("Server started on port http://localhost:8080"),
+    const PORT = process.env.PORT || 8080;
+    app.listen(PORT, () =>
+      console.log(`Server started on port http://localhost:${PORT}`),
     );
   } catch (error) {
     console.log(error);
